251e991a3fe168106bc6b3ab6953ff9429e88c92
NoReply Amazon <no-reply@amazon.com>
2024-12-09 03:56:07 +0000

    [Commit] Iteration 10 #errors = 54 <= 58.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/Source/LoreSoft.MathExpressions.Tests/UnitConversion/LengthConverterTest.cs][CS0117][29,20] 'Assert' does not contain a definition for 'AreEqual'``.

--- Source/LoreSoft.MathExpressions.Tests/UnitConversion/LengthConverterTest.cs
+++ Source/LoreSoft.MathExpressions.Tests/UnitConversion/LengthConverterTest.cs
@@ -26,19 +26,19 @@ namespace LoreSoft.MathExpressions.Tests.UnitConversion
         {
             double result = LengthConverter.Convert(
                 LengthUnit.Feet, LengthUnit.Inch, 1);
-            Assert.AreEqual(12, result);
+            Assert.That(result, Is.EqualTo(12));
 
             result = LengthConverter.Convert(
                 LengthUnit.Inch, LengthUnit.Feet, 12);
-            Assert.AreEqual(1, result);
+            Assert.That(result, Is.EqualTo(1));
 
             result = LengthConverter.Convert(
                 LengthUnit.Mile, LengthUnit.Kilometer, 10);
-            Assert.AreEqual(16.09344, result);
+            Assert.That(result, Is.EqualTo(16.09344).Within(0.000001));
 
             result = LengthConverter.Convert(
                 LengthUnit.Meter, LengthUnit.Feet, 10);
-            Assert.AreEqual(32.808398950131235, result);
+            Assert.That(result, Is.EqualTo(32.808398950131235).Within(0.000000001));
         }
     }
 }
