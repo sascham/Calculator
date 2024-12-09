de77d300bb432763bf26b80e11b2e1f5e748ab59
NoReply Amazon <no-reply@amazon.com>
2024-12-09 03:55:44 +0000

    [Commit] Iteration 9 #errors = 58 <= 62.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/Source/LoreSoft.MathExpressions.Tests/UnitConversion/MassConverterTest.cs][CS0117][29,20] 'Assert' does not contain a definition for 'AreEqual'``.

--- Source/LoreSoft.MathExpressions.Tests/UnitConversion/MassConverterTest.cs
+++ Source/LoreSoft.MathExpressions.Tests/UnitConversion/MassConverterTest.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using LoreSoft.MathExpressions.UnitConversion;
 using NUnit.Framework;
+using NUnit.Framework.Legacy;
 
 namespace LoreSoft.MathExpressions.Tests.UnitConversion
 {
@@ -26,19 +27,19 @@ namespace LoreSoft.MathExpressions.Tests.UnitConversion
         {
             double result = MassConverter.Convert(
                 MassUnit.Ounce, MassUnit.Pound, 12);
-            Assert.AreEqual(0.75, result);
+            Assert.That(result, Is.EqualTo(0.75).Within(0.000001));
 
             result = MassConverter.Convert(
                 MassUnit.Pound, MassUnit.Ounce, 0.75);
-            Assert.AreEqual(12, result);
+            Assert.That(result, Is.EqualTo(12).Within(0.000001));
 
             result = MassConverter.Convert(
                 MassUnit.Kilogram, MassUnit.Pound, 1);
-            Assert.AreEqual(2.2046226218487757d, result);
+            Assert.That(result, Is.EqualTo(2.2046226218487757d).Within(0.000001));
 
             result = MassConverter.Convert(
                 MassUnit.Ton, MassUnit.Pound, 1);
-            Assert.AreEqual(2000d, result);
+            Assert.That(result, Is.EqualTo(2000d).Within(0.000001));
         }
     }
 }
